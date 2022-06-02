d3aaa2eaecc9ae05898c5755b44cd47d87f0f3a7
Rakha HP <rpramudito@gmail.com>
2022-06-02 23:03:42 +0700

    Ini adalah solusi Challenge 04

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = function (dates, posisi) {
+  let dateParsed = [];
+  for (i = 0; i < dates.length; i++) {
+    dateParsed.push(Date.parse(dates[i]));
+    dateParsed[i] = dateParsed[i] / 1000;
+  }
+  dateParsed.sort;
+  if (posisi != null) {
+    return dateParsed[posisi].toString();
+  } else {
+    return dateParsed.toString();
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
